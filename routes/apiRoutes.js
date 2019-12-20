127169ea1c27590252869f4195d90a69aeb2ab77
gdpettus <gabe.pettus@gmail.com>
2019-12-19 20:58:46 -0700

    hooked up routes for adult and student

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,18 +2,11 @@ const db = require("../models");
 
 const test = true;
 
-const test = true;
-
 module.exports = function(app) {
   // Get all
   app.get("/api", function(req, res) {
-<<<<<<< HEAD
     db.Example.findAll({}).then(function(results) {
       res.json(results);
-=======
-    db.Example.findAll({}).then(function(athdb) {
-      res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       if (test) {
         console.log('get all')}
     });
@@ -25,13 +18,8 @@ module.exports = function(app) {
 
     //get all teams
     app.get("/api/team", function(req, res) {
-<<<<<<< HEAD
       db.Team.findAll({where: { id: req.params.team }}).then(function(results) {
         res.json(results);
-=======
-      db.Example.findAll({where: { id: req.params.team }}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
         if (test) {
           console.log('get all teams with')};
       });
@@ -39,25 +27,15 @@ module.exports = function(app) {
 
     //get one team
     app.get("/api/team/:id", function(req, res) {
-<<<<<<< HEAD
       db.Team.findOne({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //post one team
     app.post("/api/:team", function(req, res) {
-<<<<<<< HEAD
       db.Team.create({where: { id: req.params.team }}).then(function(results) {
         res.json(results);
-=======
-      db.Example.create({where: { id: req.params.team }}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
         if (test) {
           console.log('post new team')};
       });
@@ -65,25 +43,15 @@ module.exports = function(app) {
 
     //delete a team by team id. May need to change id header to match 
     app.delete("/api/team/:id", function(req, res) {
-<<<<<<< HEAD
       db.Team.destroy({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //update one team by team id
     app.put("/api/team/:id", function(req, res) {
-<<<<<<< HEAD
       db.Team.put({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.put({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
@@ -95,14 +63,9 @@ module.exports = function(app) {
     
     //get All Students
     app.get("/api/student", function(req, res) {
-<<<<<<< HEAD
       db.Student.findAll({}).then(function(results) {
         res.json(results);
         console.log('get all students',results);
-=======
-      db.Example.findAll({}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
         if (test) {
           console.log('get all students')};
       });
@@ -110,13 +73,8 @@ module.exports = function(app) {
 
     //get one student
     app.get("/api/student/:id", function(req, res) {
-<<<<<<< HEAD
       db.Student.findOne({where: { id: req.params.id }}).then(function(results) {
         res.json(results);
-=======
-      db.Example.findOne({where: { id: req.params.id }}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
         if (test) {
           console.log('get one student')};
       });
@@ -124,13 +82,8 @@ module.exports = function(app) {
 
     //post one student
     app.post("/api/student", function(req, res) {
-<<<<<<< HEAD
       db.Student.create({}).then(function(results) {
         res.json(results);
-=======
-      db.Example.create({}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
         if (test) {
           console.log('post new team')};
       });
@@ -138,25 +91,15 @@ module.exports = function(app) {
 
     //delete a student by student id. May need to change id header to match 
     app.delete("/api/student/:id", function(req, res) {
-<<<<<<< HEAD
       db.Student.destroy({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //update one student
     app.put("/api/student/:id", function(req, res) {
-<<<<<<< HEAD
       db.Student.put({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.put({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
@@ -167,61 +110,36 @@ module.exports = function(app) {
     
     //get All adults
     app.get("/api/adult", function(req, res) {
-<<<<<<< HEAD
       db.Adult.findAll({}).then(function(results) {
         res.json(results);
-=======
-      db.Example.findAll({}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //get one adult
     app.get("/api/adult/:id", function(req, res) {
-<<<<<<< HEAD
       db.Adult.findOne({where: { id: req.params.id }}).then(function(results) {
         res.json(results);        
-=======
-      db.Example.findOne({where: { id: req.params.id }}).then(function(athdb) {
-        res.json(athdb);        
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //post one adult
     app.post("/api/adult", function(req, res) {
-<<<<<<< HEAD
       db.Adult.create({}).then(function(results) {
         res.json(results);
-=======
-      db.Example.create({}).then(function(athdb) {
-        res.json(athdb);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //delete an adult by adult id. May need to change id header to match 
     app.delete("/api/adult/:id", function(req, res) {
-<<<<<<< HEAD
       db.Adult.destroy({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 
     //update one adult
     app.put("/api/adult/:id", function(req, res) {
-<<<<<<< HEAD
       db.Adult.put({ where: { id: req.params.id } }).then(function(results) {
         res.json(results);
-=======
-      db.Example.put({ where: { id: req.params.id } }).then(function(dbExample) {
-        res.json(dbExample);
->>>>>>> c3ccf3704fab94f3e3f7b00681f2e18d3e8171d8
       });
     });
 };
