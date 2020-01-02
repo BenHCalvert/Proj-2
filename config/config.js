743b370b510c9011e7394c11a02a42728afa2e2f
ferb00000100 <fhlh@protonmail.com>
2020-01-02 15:46:59 -0700

    added sms button and testing text message

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.MYSQL_PSWD,
+    "password": "root",
     "database": "sunnyside_db",
     "host": "localhost",
     "port": 3306,
