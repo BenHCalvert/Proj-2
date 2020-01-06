0d7a0caeb92cb330d6b1dd8fb022ab709d2d4a4c
gdpettus <gabe.pettus@gmail.com>
2020-01-05 21:56:10 -0700

    clean up in server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const db = require("./models");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -38,9 +39,10 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
+  console.log("here");
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+  app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
@@ -49,25 +51,4 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
-
-// cant this to work. ls works on the path but when i try to run it it cant find the path
-/*jjjjjjjjjjjjjjjj
-.then(function() {
-  // this then statement can be removed when we move to rpduction (or add an if when running dev)
-  console.log("after sequelize has initialized populating the database with seed data")
-
-  // exec(`${process.env.RUN_MYSQL}${process.env.MYSQL_PSWD} < ./models/seed.sql`,
-  // exec(`echo ${process.env.RUN_MYSQL}${process.env.MYSQL_PSWD}`,
-  exec(`cd ${process.env.MYPATH}; ls`,
-      (error, stdout, stderr) => {
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-          if (error !== null) {
-              console.log(`exec error: ${error}`);
-          }
-  });
-});
-*/
-
 module.exports = app;
-
