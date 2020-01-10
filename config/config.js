9e0714e93cb076c465343670fb812e1f19484b98
Ryan <louisalamone@gmail.com>
2020-01-09 18:49:05 -0700

    new password button nav

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: "doittoit",
+    password: process.env.MYSQL_PSWD,
     database: 'sunnyside_db',
     host: 'localhost',
     port: 3306,
