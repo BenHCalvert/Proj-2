db3256d4967d4c68e5dd35ade7254723c6965f25
Bob Benza <bbenza366@gmail.com>
2020-01-09 21:32:12 -0700

    working calendar

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,27 +1,13 @@
 var events = [
-    {'Date': new Date(2019, 11, 17), 'Title': 'RDV at 1:45pm.'},
-    {'Date': new Date(2019, 12, 18), 'Title': 'Game!'},
-    {'Date': new Date(2020, 1, 27), 'Title': 'event'},
+    // {'Date': new Date(2019, 11, 17), 'Title': 'RDV at 1:45pm.'},
+    // {'Date': new Date(2019, 12, 18), 'Title': 'Game!'},
+    // {'Date': new Date(2020, 1, 27), 'Title': 'event'},
   ];
   var settings = {};
   var element = document.getElementById('caleandar');
   caleandar(element, events, settings);
-
-
-
- 
-
   $(document).ready(function() {
-
-    
-
-   
-  
-    
-   
-    
-    
-    function curtime() {
+function curtime() {
         var now = moment().format("MMMM Do YYYY, h:mm:ss a");
         // var hour = moment().format("HH");
         
